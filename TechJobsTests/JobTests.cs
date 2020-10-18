b267ed2cc201e3a6a704bc57ed9e43fa75130cc6
Randy Parker <randallbparker@gmail.com>
2020-10-18 13:44:06 -0500

    added unit test code

---
+++ TechJobsTests/JobTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TechJobsTests
+{
+    [TestClass]
+    public class JobTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
