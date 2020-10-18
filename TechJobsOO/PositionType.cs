ed418b95f144bed5a9706e618c0e276435c8c6a2
Randy Parker <randallbparker@gmail.com>
2020-10-18 13:29:26 -0500

    positiontype and job classes updated

--- TechJobsOO/PositionType.cs
+++ TechJobsOO/PositionType.cs
@@ -18,6 +18,22 @@ namespace TechJobsOO
             Value = value;
         }
 
-        // TODO: Add custom Equals(), GetHashCode(), and ToString() methods.
+        public override bool Equals(object obj)
+        {
+            return obj is PositionType type &&
+                   Id == type.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+
+        // DONE: Add custom Equals(), GetHashCode(), and ToString() methods.
     }
 }
